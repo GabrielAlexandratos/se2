897b4ca2a33ad79a67983c8577b74ce514a83d90
Gabriel <gabriel.alexandratos@education.nsw.gov.au>
2026-03-16 09:22:27 +1100

    turned the song names on the songs page into download links for each song

--- roster.php
+++ roster.php
@@ -19,45 +19,26 @@ session_start();
                 <img src="/images/cestlavie_pfp.png" width="220" alt="cestlavie">
                 <p class="artistright"></p>
                 <p class="artisttitle">C'est la vie | 2025</p>
-                <br>
                 <p class="artistinfo">Releases:</p>
                 <p> - summer demo (2025)</p>
-            </div>
-
-            <div class="artistcontainer">
-                <img src="/images/ourdesperatelove_pfp.png" width="220" alt="ourdesperatelove">
-                <p class="artisttitle">iwillcommitsuicideattheverypeakofourdesperatelove | 2025</p>
                 <br>
-                <p class="artistinfo">Releases:</p>
-                <p> - demo (2025)</p>
-                <p> - three way split (2025)</p>
             </div>
 
             <div class="artistcontainer">
                 <img src="/images/amateurstanding_pfp.png" width="220" alt="amateurstanding">
                 <p class="artisttitle">Amateur Standing | 2025</p>
-                <br>
                 <p class="artistinfo">Releases:</p>
                 <p> - Amateur Standing (2025)</p>
+                <br>
             </div>
 
             <div class="artistcontainer">
                 <img src="/images/bughug_pfp.png" width="220" alt="bughug">
                 <p class="artisttitle">bughug | 2025</p>
-                <br>
                 <p class="artistinfo">Releases:</p>
                 <p> - _ViXtimZ_Of_BiOlogy_ (2025)</p>
                 <p> - _smth about funerals, ig (2025)</p>
                 <p> - where we flew (2025)</p>
-                <p> - three way split (2025)</p>
-            </div>
-
-            <div class="artistcontainer">
-                <img src="/images/shorthike_pfp.png" width="220" alt="shorthiketwoludo">
-                <p class="artisttitle">short hike two ludonarrative dissonance | 2025</p>
-                <br>
-                <p class="artistinfo">Releases:</p>
-                <p> - Mario Cart (2025)</p>
                 <p> - three way split (2025)</p>
             </div>
 
--- songs.php
+++ songs.php
@@ -60,7 +60,8 @@ $songs = $stmt->fetchAll(PDO::FETCH_ASSOC);
       <?php endif; ?>
 
         <div class="song-row">
-          <a class="song-title" href="song.php?id=<?= $s['id'] ?>"><?= $s['title'] ?></a>
+          <a class="song-title" href="<?= $s['audio_file'] ?>" download> <?= $s['title'] ?>
+          </a>
           <span class="song-genre">|| Genre: <?= $s['genre'] ?></span>
         </div>
 
